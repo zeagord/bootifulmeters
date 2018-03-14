042c639bfba4c7ec001cc7b7511904bf41fb5718
zeagord <sundarsy@gmail.com>
2018-03-14 15:54:52 +0700

    Initial Commit

---
+++ src/test/kotlin/com/bytesville/monitoring/bookstore/BookstoreApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.bytesville.monitoring.bookstore
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
