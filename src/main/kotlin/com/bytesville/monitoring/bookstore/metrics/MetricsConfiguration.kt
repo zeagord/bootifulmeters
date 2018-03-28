c9f06417b8e91c45ac430a0ccbc59da825e8ad27
zeagord <sundarsy@gmail.com>
2018-03-28 22:50:36 +0700

    Added custom Timer

--- src/main/kotlin/com/bytesville/monitoring/bookstore/metrics/MetricsConfiguration.kt
+++ src/main/kotlin/com/bytesville/monitoring/bookstore/metrics/MetricsConfiguration.kt
@@ -1,13 +1,30 @@
 package com.bytesville.monitoring.bookstore.metrics
 
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Timer
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics
+import java.util.concurrent.TimeUnit
 
 @Configuration
-class MetricsConfiguration {
+class MetricsConfiguration(private var registry: MeterRegistry) {
 
+  fun MetricsConfiguration(registry:MeterRegistry) {
+    this.registry = registry
+  }
   @Bean fun cvmMemoryMetrics():ClassLoaderMetrics {
     return ClassLoaderMetrics()
   }
+
+  var timer: Timer = Timer.builder("my")
+      .description("custom timer")
+      .tags("custom", "function")
+      .register(registry)
+
+  var temp  = timer.record(
+      {
+        TimeUnit.MILLISECONDS.sleep(1500)
+      }
+  )
 }
